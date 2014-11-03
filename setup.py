68f3e7741f5d3e3c04658574537e2d6fc00d311a
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-11-03 08:33:48 +0100

    version 1.0.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import numpy
 
 metadata = dict(
   name = "bitstream",
-  version = "1.0.2",
+  version = "1.0.3",
   description = "A Binary Data Type with a Stream Interface",
   url = "https://github.com/boisgera/bitstream",
   author = u"Sébastien Boisgérault",
