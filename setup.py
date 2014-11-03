e91399dae8c18266dd6e4c7d9710d347dec8491e
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-11-03 08:38:27 +0100

    setup.py test

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import numpy
 
 metadata = dict(
   name = "bitstream",
-  version = "2.0.2",
+  version = "2.0.3-alpha.1",
   description = "A Binary Data Type with a Stream Interface",
   url = "https://github.com/boisgera/bitstream",
   author = u"Sébastien Boisgérault",
@@ -152,5 +152,10 @@ if __name__ == "__main__":
     kwargs.update(metadata)
     kwargs.update(contents)
     kwargs.update(commands)
+
+    print "setup",
+    for k, n in kwargs.items():
+        print "*", k, n
+
     setuptools.setup(**kwargs)
 
