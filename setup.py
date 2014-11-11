43ceda4cb1bc7eaad53a30c0a69a4c7768e71f62
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-11-11 22:36:38 +0100

    preliminary pxd support

--- setup.py
+++ setup.py
@@ -155,6 +155,13 @@ commands = dict(
 #
 if __name__ == "__main__":
 
+    # TODO: transform bitstream into a package, include bitstream.pxd as
+    # package data, provide some API to get the path of the pxd file
+    # (such as get_pxd or get_include ? find a project that already does that, 
+    # such as lxml). Then, in the cython packages that depend on the pxd files,
+    # install bitstream as a setup dependency, query the path of the pxd file,
+    # then specify the path with cythonize (i hope that the API allows it).
+
     # CYTHON and REST options management (from command-line)
     if "--cython" in sys.argv:
         sys.argv.remove("--cython")
