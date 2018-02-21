0587d38967c858db2879ed40e7e21c50b49cf4fc
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2018-02-21 17:47:21 +0100

    declare Python 3 in setup.py

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ metadata = dict(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.6",
     "Programming Language :: Cython",
     ]
 )
