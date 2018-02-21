184951782a6aa1b9407a27f7b2a0fe47b176e315
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2018-02-21 15:58:50 +0100

    improve test runner

--- test.py
+++ test.py
@@ -8,26 +8,29 @@ import sys
 # Third-Party Libraries
 import yaml
 
-# ------------------------------------------------------------------------------
 
+# Test Files
+# ------------------------------------------------------------------------------
 mkdocs_pages = yaml.load(open("mkdocs.yml"))["pages"]
 mkdocs_files = ["mkdocs/" + list(item.values())[0] for item in mkdocs_pages]
 extra_testfiles = []
-
 test_files = mkdocs_files + extra_testfiles
+
+
+# Run the Tests
+# ------------------------------------------------------------------------------
+verbose = "-v" in sys.argv or "--verbose" in sys.argv
 
 fails = 0
 tests = 0
 for filename in test_files:
     # TODO enable verbose mode in each testfile call if appropriate
-    _fails, _tests = doctest.testfile(filename, module_relative=False)
+    options = {"module_relative": False, "verbose": verbose}
+    _fails, _tests = doctest.testfile(filename, **options)
     fails += _fails
     tests += _tests
 
-# TODO: change the behavior here: print the summary if there are errors,
-#       even if the mode is not verbose.
-verbose = "-v" in sys.argv or "--verbose" in sys.argv
-if verbose:
+if fails > 0 or verbose:
    print()
    print(60*"-")
    print("Test Suite Report:", end="")
